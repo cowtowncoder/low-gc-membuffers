86ca8fc9291ec88e514150b7e4309ae7a63f92ee
Tatu Saloranta <tsaloranta@gmail.com>
2011-06-07 21:23:07 -0700

    Completing initial read implementation

---
+++ src/test/java/com/fasterxml/membuf/MembufTestBase.java
@@ -0,0 +1,7 @@
+package com.fasterxml.membuf;
+
+import junit.framework.TestCase;
+
+public abstract class MembufTestBase extends TestCase
+{    
+}
---
+++ src/test/java/com/fasterxml/membuf/SimpleAppendRead.java
@@ -0,0 +1,9 @@
+package com.fasterxml.membuf;
+
+public class SimpleAppendRead extends MembufTestBase
+{
+    public void testX() throws Exception
+    {
+        
+    }
+}
